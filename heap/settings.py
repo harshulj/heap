c140e2afc56bfe1df9a7ca52a3bfe33c31350459
Harshul Jain <harshulj@gmail.com>
2012-12-02 17:02:02 +0530

    Added static dir, added admin link, linking to django admin static files in python site dir.

--- heap/settings.py
+++ heap/settings.py
@@ -64,12 +64,14 @@ STATIC_ROOT = ''
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
+ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    '/home/harshulj/code/django/heap/static/',
 )
 
 # List of finder classes that know how to find static files in
