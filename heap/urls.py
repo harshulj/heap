4b00bad78e3bd21f9ecd9c791414221d1ff8ef5f
Harshul Jain <harshulj@gmail.com>
2012-12-02 19:47:56 +0530

    Added bootstrap files.

--- heap/urls.py
+++ heap/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from blog.views import index
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -8,7 +9,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'h.views.home', name='home'),
     # url(r'^heap/', include('heap.foo.urls')),
-
+    url(r'^$', index),
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
